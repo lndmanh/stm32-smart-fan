342841e9115cfd957455c8f4dcffaa2439d68b1a
Le Ngo Duc Manh <myt@nnsvn.me>
2026-06-11 15:06:19 +0700

    Add UART command helpers

--- Core/Inc/hardware.h
+++ Core/Inc/hardware.h
@@ -5,5 +5,9 @@
 
 void System_Init(void);
 void Set_Motor_Output(int32_t u);
+void UART2_Init(void);
+int UART2_ReadChar(char *ch);
+void UART2_WriteChar(char ch);
+void UART2_WriteString(const char *text);
 
 #endif /* HARDWARE_H */
--- Core/Src/hardware.c
+++ Core/Src/hardware.c
@@ -5,6 +5,8 @@
 #define PWM_FREQ        10000U
 #define PWM_ARR         ((SYS_CLOCK / PWM_FREQ) - 1U)
 
+#define UART_BAUD_RATE 115200U
+
 void Set_Motor_Output(int32_t u) {
     if (u >= 0) {
         GPIOA->ODR |= (1U << 9);
@@ -21,11 +23,13 @@ void System_Init(void) {
     RCC->AHB1ENR |= RCC_AHB1ENR_GPIOAEN;
     RCC->AHB1ENR |= RCC_AHB1ENR_GPIOBEN;
     RCC->APB2ENR |= RCC_APB2ENR_TIM1EN;
-    RCC->APB1ENR |= RCC_APB1ENR_TIM2EN | RCC_APB1ENR_TIM3EN;
+    RCC->APB1ENR |= RCC_APB1ENR_TIM2EN | RCC_APB1ENR_TIM3EN | RCC_APB1ENR_USART2EN;
 
     GPIOA->MODER &= ~(
         (3U << (0U * 2U)) |
         (3U << (1U * 2U)) |
+        (3U << (2U * 2U)) |
+        (3U << (3U * 2U)) |
         (3U << (8U * 2U)) |
         (3U << (9U * 2U)) |
         (3U << (10U * 2U))
@@ -33,12 +37,14 @@ void System_Init(void) {
     GPIOA->MODER |=
         (2U << (0U * 2U)) |
         (2U << (1U * 2U)) |
+        (2U << (2U * 2U)) |
+        (2U << (3U * 2U)) |
         (2U << (8U * 2U)) |
         (1U << (9U * 2U)) |
         (1U << (10U * 2U));
 
-    GPIOA->AFR[0] &= ~((15U << 0) | (15U << 4));
-    GPIOA->AFR[0] |= (1U << 0) | (1U << 4);
+    GPIOA->AFR[0] &= ~((15U << 0) | (15U << 4) | (15U << 8) | (15U << 12));
+    GPIOA->AFR[0] |= (1U << 0) | (1U << 4) | (7U << 8) | (7U << 12);
 
     GPIOA->AFR[1] &= ~(15U << 0);
     GPIOA->AFR[1] |= (1U << 0);
@@ -60,4 +66,36 @@ void System_Init(void) {
     TIM3->DIER |= TIM_DIER_UIE;
     NVIC_EnableIRQ(TIM3_IRQn);
     TIM3->CR1 |= TIM_CR1_CEN;
+
+    UART2_Init();
+}
+
+void UART2_Init(void) {
+    USART2->CR1 = 0U;
+    USART2->CR2 = 0U;
+    USART2->CR3 = 0U;
+    USART2->BRR = (SYS_CLOCK + (UART_BAUD_RATE / 2U)) / UART_BAUD_RATE;
+    USART2->CR1 = USART_CR1_TE | USART_CR1_RE | USART_CR1_UE;
+}
+
+int UART2_ReadChar(char *ch) {
+    if ((USART2->SR & USART_SR_RXNE) == 0U) {
+        return 0;
+    }
+
+    *ch = (char)(USART2->DR & 0xFFU);
+    return 1;
+}
+
+void UART2_WriteChar(char ch) {
+    while ((USART2->SR & USART_SR_TXE) == 0U) {
+    }
+
+    USART2->DR = (uint32_t)(uint8_t)ch;
+}
+
+void UART2_WriteString(const char *text) {
+    while (*text != '\0') {
+        UART2_WriteChar(*text++);
+    }
 }
